19520a3d317f99dcd849f0d4508d391bdc85ac41
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-03-18 08:24:15 -0400

    category route with include product

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,9 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
